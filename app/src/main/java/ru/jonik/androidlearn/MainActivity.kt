d417f5ced253cd43b246ebc3b071099b9f13d1d2
JonikBoltaev <boltaevjahongiral@gmail.com>
2022-03-11 22:09:19 +0300

    Array Adapter

--- app/src/main/java/ru/jonik/androidlearn/MainActivity.kt
+++ app/src/main/java/ru/jonik/androidlearn/MainActivity.kt
@@ -1,94 +1,99 @@
 package ru.jonik.androidlearn
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Adapter
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.SimpleAdapter
 import androidx.appcompat.app.AlertDialog
 import ru.jonik.androidlearn.databinding.ActivityMainBinding
+import ru.jonik.androidlearn.databinding.DialogAddCharacterBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: ArrayAdapter<Character>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setupListViewWithSimpleGeneratedData()
+
+        setupListViewWithArrayAdapter()
+        binding.btnAdd.setOnClickListener { onAddPressed() }
+
     }
 
-    // Базовая генерация даты
-    private fun setupListViewWithSimpleGeneratedData() {
-        val data = (1..100).map {
-            mapOf(
-                KEY_TITLE to "Item #$it",
-                KEY_DESCRIPTION to "Description #$it"
-            )
-        }
-        // Инициализация адаптера
-        // SimpleAdapter - Требует данные определенной структуры данных (List с ключ-значение)
-        val adapter = SimpleAdapter( // Принимает 5 аргументов
-            this,
-            data, // Передаем данные
-            //TODO
-            R.layout.item_custom, // Кастомный макет
-            arrayOf(
-                KEY_TITLE,
-                KEY_DESCRIPTION
-            ), // Передаем KEY_TITLE на text1 и так же для след элемента
-            intArrayOf(R.id.tv_title, R.id.tv_description)
+    private fun setupListViewWithArrayAdapter() {
+        val data = mutableListOf(
+            Character(id = UUID.randomUUID().toString(), name = "Sven"),
+            Character(id = UUID.randomUUID().toString(), name = "Juggernaut"),
+            Character(id = UUID.randomUUID().toString(), name = "Faceless void"),
+            Character(id = UUID.randomUUID().toString(), name = "Axe")
         )
-        // Вызов свойства adapter и присваиваем созданный адаптер
+        adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            android.R.id.text1,
+            data
+        )
+
         binding.listView.adapter = adapter
 
-        binding.listView.onItemClickListener =
-            AdapterView.OnItemClickListener { parent, view, position, id ->
-                val selectedItemTitle = data[position][KEY_TITLE]
-                val selectedItemDescription = data[position][KEY_DESCRIPTION]
-
-                val dialog = AlertDialog.Builder(this)
-                    .setTitle(selectedItemTitle)
-                    .setMessage(getString(R.string.item_selected_message, selectedItemDescription))
-                    .setPositiveButton("Ok") { dialog, which -> }
-                    .create()
-                dialog.show()
+        binding.listView.setOnItemClickListener { parent, view, position, id ->
+            adapter.getItem(position)?.let {
+                deleteCharacter(it)
             }
+        }
     }
 
-    companion object {
-        @JvmStatic
-        val KEY_TITLE = "title"
 
-        @JvmStatic
-        val KEY_DESCRIPTION = "description"
+    private fun onAddPressed() {
+        val dialogBinding = DialogAddCharacterBinding.inflate(layoutInflater)
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Create character")
+            .setView(dialogBinding.root)
+            .setPositiveButton("Add") { d, which ->
+                val name = dialogBinding.edCharacterName.text.toString()
+                if (name.isNotBlank()) {
+                    createCharacter(name)
+                }
+            }
+            .create()
+        dialog.show()
+    }
+
+    private fun createCharacter(name: String) {
+        val character = Character(
+            id = UUID.randomUUID().toString(),
+            name = name
+        )
+        adapter.add(character)
+    }
+
+    private fun deleteCharacter(character: Character) {
+        val listener = DialogInterface.OnClickListener { dialog, which ->
+            if (which == DialogInterface.BUTTON_POSITIVE) {
+                adapter.remove(character)
+            }
+        }
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Delete character")
+            .setMessage("Delete the character: $character")
+            .setPositiveButton("Delete", listener)
+            .setNegativeButton("Cancel", listener)
+            .create()
+        dialog.show()
+    }
+
+    class Character(
+        val id: String,
+        val name: String
+    ) {
+        override fun toString(): String {
+            return name
+        }
     }
 }
-
-//    private fun setupListView() {
-
-//        // Создание данных для адаптера
-//        val data: List<Map<String, String>> = listOf(
-//            mapOf(
-//                KEY_TITLE to "1 title",
-//                KEY_DESCRIPTION to "1 description"
-//            ),
-//            mapOf(
-//                KEY_TITLE to "2 title",
-//                KEY_DESCRIPTION to "2 description"
-//            ),
-//            mapOf(
-//                KEY_TITLE to "3 title",
-//                KEY_DESCRIPTION to "3 description"
-//            )
-//        )
-//        // Инициализация адаптера
-//        // SimpleAdapter - Требует данные определенной структуры данных (List с ключ-значение)
-//        val adapter = SimpleAdapter( // Принимает 5 аргументов
-//            this,
-//            data, // Передаем данные
-//            android.R.layout.simple_list_item_2, // Стандартный макетный файл для вывода каждого элемента списка. Состоит из 2-х вертикальных textView
-//            arrayOf(KEY_TITLE, KEY_DESCRIPTION), // Передаем KEY_TITLE на text1 и так же для след элемента
-//            intArrayOf(android.R.id.text1, android.R.id.text2)
-//        )
-//        // Вызов свойства adapter и присваиваем созданный адаптер
-//        binding.listView.adapter = adapter
